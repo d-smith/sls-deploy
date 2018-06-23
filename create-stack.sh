0a6549569639d3f224359fb2fbd69e6ff9ee0e96
d-smith <doug.smith.mail@gmail.com>
2018-06-23 07:18:55 -0700

    Updated create script

--- create-stack.sh
+++ create-stack.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: create-stack <stack name> <stage> <region>"
+    exit 1
+fi
+
 aws cloudformation create-stack \
---stack-name simple-deploy \
+--stack-name $1 \
 --template-body file://pipeline.yml \
+--parameters ParameterKey=StageName,ParameterValue=$2 ParameterKey=DeployRegion,ParameterValue=$3 \
 --capabilities CAPABILITY_IAM
